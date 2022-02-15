69a19c037057381833b52ef1a4d6192408758e44
matheus <mnatalsantos@hotmail.com>
2022-02-15 14:30:01 -0300

    operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,39 @@
+// Exercícios de interpretação
+ // 1) False , False , True , Boolean
+  // 2) O resultado será o par de números um ao lado do outro, sem que ambos sejam realmente somados.
+// 3) Será necessário que o colega altere o código de forma a por o objeto Number antes do método prompt para de fato realizar a soma.
+
+// Exercícios de escrita de código
+
+// Exercício 1)
+ // let idadeUsuario = Number (prompt("Qual a sua idade?"))
+  //  let idadeMelhorAmigo = prompt ("Qual a idade do seu melhor amigo ou amiga?")  
+   //   console.log("Sua idade é maior do que a do seu melhor amigo?", idadeUsuario > idadeMelhorAmigo)
+    //  console.log ("diferença de idade", idadeUsuario - idadeMelhorAmigo) 
+
+
+// Exercício 2)
+// let inserirNumeroPar = Number (prompt("Insira um número par"))
+//  console.log(inserirNumeroPar % 2)
+  // c) O padrão que pude perceber é o seguinte, o "resto da divisão" reconhece como sendo par ou não através
+   // do resultado dessa divisão, caso o mesmo não seja um divisivel direto por 2, e haja necessidade de descer uma virgula
+    // o mesmo irá imprimir 1 nos mostrando que o número é impar.
+     // d) O console irá apresentar o número 1.
+
+
+    //  Exercicio 3) 
+
+// let idade = Number (prompt("Qual sua idade em anos?"))
+ //   console.log ("Idade em anos",idade * 12 )
+  //     console.log ("Idade em dias",365 * idade)
+   //      console.log("Idade em horas",8760 * idade)
+
+ //Exercicio 4)
+//  let numero1 = Number(prompt("Digite um número"))
+//   let numero2 = Number(prompt("Digite um segundo número"))
+//    console.log("O primeiro número é maior que o segundo", numero1 > numero2)
+//     console.log("O primeiro número é igual ao segundo?", numero1 === numero2)
+//      console.log("O primeiro numero é divisivel pelo segundo?", (numero1 / numero2) === 0)
+
+
+
--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,51 +1,34 @@
-/* Exercícios de Interpretação 
- Exercício 1)
-  Será impresso 10 5
-   Exercício 2)
-    Será impresso 10 10 10
-   Exercício 3)
-  Será let horasPorDia = prompt("Quantas horas você trabalha por dia?")
- let recebePorDia = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${horasPorDia/recebePorDia} por hora`)
-*/
+//const a = 18
+//const b = 19
+// console.log("O primeiro número é igual que o segundo?", a === b )
+// console.log("O primeiro número é diferente que o segundo?", a !== b)
+// console.log("O primeiro número é maior que o segundo?", a > b)
+// console.log("O primeiro número é menor que o segundo?", a < b)
 
-
-let nome 
-    let idade
-   /* typeof nome
-    typeof idade*/
-        //console.log(typeof nome )
-         //console.log(typeof idade )
-           //Deu Undefined porque não há nenhum valor atribuido a ambos.
-         nome = prompt("Qual o seu nome?")
-     idade = prompt("Qual a sua idade?")
-  console.log(typeof nome, typeof idade,)
- /*Ambos retornaram como String pois o prompt só retorna
-   desta maneira */
-console.log("Olá",nome, "Você tem", idade, "anos")
-
-//Exercício número 2
-  let perguntaRoupa = prompt("Vocẽ esta usando uma roupa azul hoje? RESPONDA APENAS SIM OU NÃO")
-    let temAnimal = prompt("Vocẽ tem um animal?")
-     let gostaDeSuco = prompt("Você gosta de suco?")
-      let viuVingadores = prompt("Você viu o filme dos vingadores?")
-     console.log("pergunta roupa azul", perguntaRoupa)
-    console.log("tem animal?",temAnimal)
-   console.log("gosta de suco?", gostaDeSuco)
-  console.log("Viu o filme dos vingadores?", viuVingadores)
-//Exercício número 3
-  let a = 10
-   let b = 25
-    let c 
-     c = a
-    a = b
-   b = c
-  console.log(a,b)
-/* Explicação  Nesta parte se fez necessário a criação de uma 
-  terceira variavel "C" para que haja a preservação do valor da variavel seguindo o exemplo 
- dos copos, presente nas dicas do Notion"*/
+// const habilitada = true
+// const maiorDeIdade = false
+// const estudante = true
 
 
 
+// console.log(!maiorDeIdade && estudante && habilitada)
 
+
+
+// receber o nome 
+// receber o ano de nascimento
+// receber o ano atual 
+// checar se é maior ou menor de idade 
+// mostrar na tela
+
+const nome = prompt("Digie o seu nome")
+ const anoNasc = prompt ("Digite o ano em que vocẽ nasceu")
+  const anoAtual = prompt("Digite o ano que estamos") 
+   let idade = anoAtual - anoNasc
+    let maiorDeIdade = idade >= 18  
+     let idadeFutura = 2050 - anoNasc 
+    console.log("nome",nome)
+  console.log("idade", idade)
+ console.log("É maior de idade", maiorDeIdade)
+console.log ("idade em 2050", idadeFutura)
 
