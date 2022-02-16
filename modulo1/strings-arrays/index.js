abff74d33962cc52fdc7c10c0a70e7a9fcf5b351
matheus <mnatalsantos@hotmail.com>
2022-02-16 16:27:39 -0300

    strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,43 @@
+// Exercicio 1
+// a) Undefined
+// b) Null
+// c) 11
+// d) item 0 da lista = 3 ( resposta 3)
+// e) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f) 8
+
+  // Exercício 2
+  //"SUBI NUM ONIBUS EM MIRROCOS" 27 letras
+  
+  // Exercícios de escrita de código
+   // 1
+    //    const nomeDoUsuario = prompt("Qual o seu nome?")
+    //     const  email = prompt("Digite o seu melhor e-mail")
+    //      console.log(`O ${email} foi cadastrado com sucesso.Seja bem-vinda(o),${nomeDoUsuario}`)
+          // Exercício 2
+            // let comidasFav = ["arroz", "feijão", "carne", "peixe", "tomate"]
+            //  const frase = ("Essas são as minhas comidas preferidas")
+            //   let comidasFavNumeros = [comidasFav]
+            //   console.log(frase)
+            //    console.log(comidasFav[0])   
+            //     console.log(comidasFav [1])
+            //      console.log(comidasFav [2])
+            //       console.log(comidasFav [3])
+            //        console.log(comidasFav [4])
+            //         // DESAFIO
+            //          let qualComidaFavorita = prompt("Qual a sua comida favorita?")
+            //           let comidasFavnova = comidasFav.replaceAll(1, qualComidaFavorita)
+
+                        // 3 Exercício
+                    //    let listaDeTarefas = []
+                    //     listaDeTarefas.push (prompt("Cite uma tarefa do seu dia a dia "))
+                    //     listaDeTarefas.push (prompt("Cite uma segunda tarefa do seu dia a dia "))
+                    //     listaDeTarefas.push (prompt("Cite uma terceira tarefa do seu dia a dia "))
+                    //     console.log("lista de tarefas",listaDeTarefas)
+                    //     let digitarIndice = prompt("Digite o índice de uma tarefa que vocẽ já realizou: 0, 1, 2")
+                    //     listaDeTarefas.splice (digitarIndice, 1)
+                    //     console.log(listaDeTarefas)
+                      
+
+                      
+                   
