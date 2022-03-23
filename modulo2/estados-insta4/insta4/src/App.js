751797aedc1e228ee809bb814a4de9e8c77168ae
matheus <mnatalsantos@hotmail.com>
2022-03-23 16:16:03 -0300

    parei na curtida coracao preto e branco add 1 tira 1

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/151'}
+        />
+
+<Post
+          nomeUsuario={'fulano'}
+          fotoUsuario={'https://picsum.photos/50/52'}
+          fotoPost={'https://picsum.photos/200/152'}
+        />
+
+<Post
+          nomeUsuario={'beltrano'}
+          fotoUsuario={'https://picsum.photos/50/53'}
+          fotoPost={'https://picsum.photos/200/153'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
