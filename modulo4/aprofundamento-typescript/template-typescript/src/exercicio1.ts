36cbcff03480ea740d56e2748ed7c86442f6863e
matheus <mnatalsantos@hotmail.com>
2022-05-26 23:05:07 -0300

    exercicio 1, 2 inconpleto iniciar o 3

---
+++ modulo4/aprofundamento-typescript/template-typescript/src/exercicio1.ts
@@ -0,0 +1,48 @@
+
+// R = Resposta
+
+// A) const minhaString:string =3
+//   R: Me é informado que não pode ser atribuido um número ao tipo String.
+
+
+ // B) const meuNumero:number | string = 10
+ //   R:  Para que uma variável aceite mais de um tipo de valor é
+ // necessário  o uso de Union type
+
+
+// C) const pesssoa : {nome:string, idade:number, corFavorita:string}={
+//      nome:"Fulano",
+//      idade: 26,
+//      corFavorita:"Azul"
+//  }
+
+
+//  type pessoa = {
+//      nome:"",
+//      idade:1,
+//      corFavorita:""
+//  }
+
+//  type pessoa2 ={
+//     nome:"",
+//     idade:1,
+//     corFavorita:""
+// }
+
+// type pessoa3 ={
+//     nome:"",
+//     idade:1,
+//     corFavorita:""
+// }
+
+// D)
+
+// enum corFavorita{
+//     VIOLETA="violeta",
+//     ANIL="anil",
+//     AZUL="azul",
+//     VERDE="verde",
+//     AMARELO="amarelo",
+//     LARANJA="laranja",
+//     VERMELHO="vermelho"
+// }
