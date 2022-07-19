af25b11058f94ca9f7d1e4ff4bbb81784a6b4f19
matheus <mnatalsantos@hotmail.com>
2022-07-18 21:58:37 -0300

    labefoods

---
+++ modulo6/labfoods/src/Pages/SingUp/singUp.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const singUp = () =>{
+    return(
+        <div>singUp</div>
+    )
+}
+export default singUp
---
+++ modulo6/labfoods/src/Pages/SingUpAdress/singUpAdress.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const singUpAdress = () =>{
+    return(
+        <div>singUpAdress</div>
+    )
+}
+export default singUpAdress
