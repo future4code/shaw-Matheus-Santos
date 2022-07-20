f8ea235a4b42ef2fff5c197fd3b27fb0ae82c674
matheus <mnatalsantos@hotmail.com>
2022-07-20 03:01:12 -0300

    hook-useRequestData e useProtectedPage

--- modulo6/labfoods/src/Pages/Feed/feed.js
+++ modulo6/labfoods/src/Pages/Feed/feed.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useProtectedPage } from "../../Hooks/useProtectedPage";
+import { BASE_URL } from "../../Constants/url";
+import axios from "axios";
+
 
 const Feed = () =>{
+
+
+    useProtectedPage()
+
     return(
         <div>Feed</div>
     )
