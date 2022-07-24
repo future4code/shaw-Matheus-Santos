81206fa230f0ffefd27d4a02481a5827d1c2aaa4
matheus <mnatalsantos@hotmail.com>
2022-07-24 16:22:39 -0300

    home e estilização

--- modulo6/labfoods/src/Pages/Feed/feed.js
+++ modulo6/labfoods/src/Pages/Feed/feed.js
@@ -1,16 +1,49 @@
 import React, {useEffect, useState} from "react";
-import { useProtectedPage } from "../../Hooks/useProtectedPage";
-import { BASE_URL } from "../../Constants/url";
 import axios from "axios";
+import { BASE_URL } from "../../Constants/url";
+import { CardsRestaurant, ContainerFeed, InputSearch } from "./styled";
+import CardRestaurant from "../../Components/CardRestaurant/CardRestaurant";
+import Header from "../../Components/Header/CardRestaurant/Header";
 
 
 const Feed = () =>{
 
+    const [restaurants, setRestaurants] = useState([])
 
-    useProtectedPage()
+    const getRestaurants = () => {
+        axios.get(`${BASE_URL}/restaurants`,{
+            headers:{
+                auth:localStorage.getItem("token")
+            }
+        })
+        .then((res) => {
+            console.log(res.data);
+            setRestaurants(res.data.restaurants)
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    useEffect(()=>{
+        getRestaurants()
+    }, [])
+
+    
 
     return(
-        <div>Feed</div>
+        <ContainerFeed>
+            <Header title={"Ifuture"}/>
+            <CardsRestaurant>
+            <InputSearch/>
+                {
+                    restaurants.map((restaurant)=>{
+                        return <CardRestaurant 
+                        restaurant={restaurant}
+                        />
+                    })
+                }
+            </CardsRestaurant>
+        </ContainerFeed>
     )
 }
 export default Feed
