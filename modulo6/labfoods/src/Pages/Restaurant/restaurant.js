5b10ddad08271a2249f72638a61533ae287fa7b4
matheus <mnatalsantos@hotmail.com>
2022-07-31 02:32:17 -0300

    entrando no carrinho...

--- modulo6/labfoods/src/Pages/Restaurant/restaurant.js
+++ modulo6/labfoods/src/Pages/Restaurant/restaurant.js
@@ -4,11 +4,12 @@ import { useParams } from "react-router-dom";
 import CardProduct from "../../Components/CardProduct/CardProduct";
 import CardRestaurant from "../../Components/CardRestaurant/CardRestaurant";
 import { BASE_URL } from "../../Constants/url";
-import { CardRestaurant, ContainerRestaurant } from "./styled";
+import { CardRestaurant, Category, ContainerRestaurant, SectionProductByCategory } from "./styled";
 
 const Restaurant = () =>{
     const { restaurantId } =useParams()
-    const [restaurant, setRestaurant] =useState([])
+    const [restaurant, setRestaurant] =useState({})
+    const [categories, setCategories] =useState([])
     const getRestaurants = () =>{
         axios.get(`${BASE_URL}/restaurants/${restaurantId}`,{
             headers:{
@@ -26,15 +27,47 @@ const Restaurant = () =>{
     useEffect(()=> {
         getRestaurants()
     }, [])
+    useEffect(()=>{
+        if(restaurant.products) {
+            const newCategories = []
+            for( const product of restaurant.products) {
+                if(!newCategories.includes(product.category)){
+                     newCategories.push(product.category);
+                }
+            }
+            setCategories(newCategories )
+        }
+    }, [restaurant])
     return(
         <ContainerRestaurant>
             <CardRestaurant>
                 <CardRestaurantDetails restaurant = {restaurant} />
+                
                 {
+                    restaurant.products
+                    &&
+                    categories.map((category)=>{
+                        return <SectionProductByCategory>
+                            <Category>{category}</Category>
+                            {
+                                restaurant.products
+                                .filter((product)=>{
+                                    return product.category === category
+                                })
+                                .map((product)=>{
+                                    return <CardProduct product={product} key={product.id}
+                                    />
+                                })
+                            }
+                        </SectionProductByCategory>
+                    })
+                }
+
+                {/* {
                     restaurant.products && restaurant.products.map((product) => {
                         return <CardProduct product = {product} key={product.id}/>
                     })
-                }
+                } */}
             </CardRestaurant>
         </ContainerRestaurant>
     )
